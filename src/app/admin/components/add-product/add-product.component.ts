2e1111242ee7abf024a0ee037fab03e34b552347
DLinh <nogamenolife.why@gmail.com>
2022-05-18 16:30:41 +0700

    refactor: change link product type

--- src/app/admin/components/add-product/add-product.component.ts
+++ src/app/admin/components/add-product/add-product.component.ts
@@ -1,4 +1,4 @@
-import { IProduct } from './../../../models/Product';
+import { IProduct } from '../../../models/product.model';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
