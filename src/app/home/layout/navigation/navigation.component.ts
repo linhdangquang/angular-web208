6c7b9dbd87099171abf97ccf543c0391cc40dd32
DLinh <nogamenolife.why@gmail.com>
2022-06-02 07:08:05 +0700

    change navigation styles

--- src/app/home/layout/navigation/navigation.component.ts
+++ src/app/home/layout/navigation/navigation.component.ts
@@ -1,15 +1,21 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { map, Observable, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent  {
 
-  constructor() { }
+  panelOpenState = false;
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 
-  ngOnInit(): void {
-  }
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
 }
